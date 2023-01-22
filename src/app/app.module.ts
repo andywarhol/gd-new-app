d5456dcddcbaa244efbe3c1ac7275a714df25474
Andrea Mariana Urrea Urias <andrea.urrea@neoris.com>
2023-01-22 00:49:22 -0700

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls:['./login.component.css']
+})
+
+export class LoginComponent {
+    /*
+    form = new FormGroup({
+        username: new FormControl(null, Validators.required)
+        password: new FormControl(null, Validators.required)
+    })
+    */
+}
